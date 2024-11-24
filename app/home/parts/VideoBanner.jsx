8babd3ba5a3fe78bd8add77aaadfa9c2e73a91d4
Goddie-Owiny <goddiegodfreyz07@gmail.com>
2024-11-24 12:35:39 +0300

    fixing responsiveness of home hero page

--- app/home/parts/VideoBanner.jsx
+++ app/home/parts/VideoBanner.jsx
@@ -1,74 +1,125 @@
-import React from 'react';
-import { Container, Button, Grid, Link, Text, Box, ButtonGroup } from '@chakra-ui/react';
-
-import Image from 'next/image';
-
+import React from "react";
+import {
+  Container,
+  Button,
+  Grid,
+  Link,
+  Text,
+  Box,
+  ButtonGroup,
+} from "@chakra-ui/react";
+import Image from "next/image";
 
 export default function VideoBanner() {
-    return (
-        <Container maxW='container.xl' position="relative" mb={5} mt={{ base: 16, lg: 18 }} pt={{base: 10, lg:20}}>
-            <Text fontWeight='bold' fontSize={{ base: '3xl', lg: '7xl'}} color='var( --clr-primary-1)' textAlign='center'>AFRICAN SIMBA EVENTS</Text>
-            <Text color='var( --clr-primary-1)' fontSize={{ base: '2xl', lg: '3xl'}} textAlign='center'>
-              " THE GAME CHANGERS"
-            </Text>
-            <Box mt={10} position="relative">
-            <div style={{ position: "relative", width: "100%", height: "600px" }}>
-  <Image
-    src="/compressed/bg_image.webp"
-    alt="night club"
-    layout="fill" // Makes the image fill the parent container
-    objectFit="cover" // Ensures the image maintains its aspect ratio and fills the area
-    style={{
-      borderRadius: "10px", // Rounded corners
-      transition: "transform 0.3s", // Smooth hover effect
-    }}
-   
-    priority // Optimized for loading
-  />
-</div>
-                {/* Gradient Overlay */}
-                <Box
-                    position="absolute"
-                    bottom="0"
-                    left="0"
-                    width="100%"
-                    height="100%"  // Adjust this value to control gradient height
-                    bgGradient="linear(to-t, rgba(0, 0, 0, 0.9), transparent)"  // Gradient from bottom to top
-                    zIndex="1"
-                    roundedBottom={10}
-                />
-                {/* Text Overlay */}
-                <Box
-                    position="absolute"
-                    bottom="0"
-                    left="0"
-                    width="100%"
-                    p={4}
-                    zIndex="2"
-                    color="var( --clr-primary-1)"
-                    borderRadius="lg"
-                >
-                   
-                   
-                        <Text fontWeight='bold' fontSize={{base: '2xl', lg: '3xl'}} color='var( --clr-primary-1)' textAlign='center'>Celebrate with the Game Changers</Text>
-                    <Text color='var( --clr-primary-1)' mb={2} textAlign='center'>
-                        Experience Immersive party that happens every week in Kampala, Uganda
-                    </Text>
-                    <Grid justifyContent='center'>
-                        <ButtonGroup spacing={8}>
-                            <Button as={Link} href='/events'
-                                _hover={{ textDecoration: 'none', bg: 'var( --clr-primary-1)', color: 'black' }}
-                                textDecoration='none' target='_blank' colorScheme="Alpha" rounded='full' variant="outline">
-                                More Information
-                            </Button>
-                            <Button as={Link} href='/bookings' _hover={{ textDecoration: 'none', bg: 'var( --clr-primary-1)', color: 'black' }}  textDecoration='none' colorScheme="Alpha" rounded='full' variant="outline">
-                               Get Ticket Now
-                            </Button>
-                        </ButtonGroup>
-                    </Grid>
-                   
-                </Box>
-            </Box>
-        </Container>
-    );
+  return (
+    <Container
+      maxW="container.xl"
+      position="relative"
+      mb={5}
+      mt={{ base: 16, lg: 18 }}
+      pt={{ base: 10, lg: 20 }}
+    >
+      {/* Header Text */}
+      <Text
+        fontWeight="bold"
+        fontSize={{ base: "3xl", lg: "7xl" }}
+        color="var(--clr-primary-1)"
+        textAlign="center"
+      >
+        AFRICAN SIMBA EVENTS
+      </Text>
+      <Text
+        color="var(--clr-primary-1)"
+        fontSize={{ base: "2xl", lg: "3xl" }}
+        textAlign="center"
+      >
+        "THE GAME CHANGERS"
+      </Text>
+
+      {/* Hero Image Section */}
+      <Box mt={10} position="relative" height={{ base: "300px", md: "600px" }}>
+        {/* Background Image */}
+        <Image
+          src="/compressed/bg_image.webp"
+          alt="night club"
+          layout="fill"
+          objectFit="cover"
+          priority // Optimized for loading
+          style={{
+            borderRadius: "10px", // Rounded corners
+            transition: "transform 0.3s", // Smooth hover effect
+            opacity: 0.5,
+          }}
+        />
+
+        {/* Gradient Overlay */}
+        <Box
+          position="absolute"
+          bottom="0"
+          left="0"
+          width="100%"
+          height="100%"
+          bgGradient="linear(to-t, rgba(0, 0, 0, 0.9), transparent)" // Gradient from bottom to top
+          zIndex="1"
+          borderRadius="10px"
+        />
+
+        {/* Text Overlay */}
+        <Box
+          position="absolute"
+          bottom="0"
+          left="0"
+          width="100%"
+          p={4}
+          zIndex="2"
+          color="var(--clr-primary-1)"
+          textAlign="center"
+        >
+          <Text
+            fontWeight="bold"
+            fontSize={{ base: "xl", lg: "3xl" }}
+            color="var(--clr-primary-1)"
+          >
+            Celebrate with the Game Changers
+          </Text>
+          <Text color="var(--clr-primary-1)" mb={4}>
+            Experience Immersive party that happens every week in Kampala,
+            Uganda
+          </Text>
+          <Grid justifyContent="center">
+            <ButtonGroup spacing={8}>
+              <Button
+                as={Link}
+                href="/events"
+                _hover={{
+                  textDecoration: "none",
+                  bg: "var(--clr-primary-1)",
+                  color: "black",
+                }}
+                rounded="full"
+                variant="outline"
+                color="var(--clr-primary-1)"
+              >
+                More Information
+              </Button>
+              <Button
+                as={Link}
+                href="/bookings"
+                _hover={{
+                  textDecoration: "none",
+                  bg: "var(--clr-primary-1)",
+                  color: "black",
+                }}
+                rounded="full"
+                variant="outline"
+                color="var(--clr-primary-1)"
+              >
+                Get Ticket Now
+              </Button>
+            </ButtonGroup>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
