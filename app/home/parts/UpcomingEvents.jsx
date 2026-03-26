445accfa47d0449c6e95c74f4f2187ec8a6d6d34
Sonoma <apple17@Graphics-2.local>
2026-03-25 17:48:06 -0700

    adding single Ibiza event

--- app/home/parts/UpcomingEvents.jsx
+++ app/home/parts/UpcomingEvents.jsx
@@ -29,13 +29,14 @@ export const UpcomingEvents = () => (
             Upcoming Event:
         </Text>
 
-        {/* Eclipse Brunch */}
+        {/* Ibiza Experience */}
         <Stack
             direction={{ base: 'column', lg: 'row' }}
             spacing={{ base: '6', md: '8', lg: '20' }}
             align={{ base: 'center', lg: 'stretch' }}
+            mt={{ base: '10', md: '14', lg: '16' }}
         >
-            {/* Images Section */}
+                   {/* Images Section */}
             <Flex
                 flex="1"
                 overflow="hidden"
@@ -71,123 +72,6 @@ export const UpcomingEvents = () => (
 
                         boxShadow: 'xl'
                     }}
-                />
-            </Flex>
-
-            {/* Content Section */}
-            <Box
-                w={{ base: '100%', lg: 'sm' }}
-                maxW={{ base: '500px', lg: 'sm' }}
-                rounded="10"
-                px={{ base: '6', md: '8', lg: '0' }}
-                py={{ base: '6', md: '8', lg: '12' }}
-                bg={{ base: 'var(--clr-primary-3)', lg: 'transparent' }}
-            >
-                <VStack
-                    spacing={{ base: '6', md: '8', lg: '10' }}
-                    align={{ base: 'center', md: 'flex-start', lg: 'flex-start' }}
-                >
-                    <Stack
-                        spacing={{ base: '3', lg: '4' }}
-                        textAlign={{ base: 'center', md: 'left', lg: 'left' }}
-                        w="100%"
-                    >
-                        <Text
-                            fontSize={{ base: 'xl', sm: '2xl', md: '3xl' }}
-                            fontWeight="bold"
-                            color="var(--clr-primary-1)"
-
-                        >
-                            THE ECLIPSE BRUNCH
-                        </Text>
-
-
-                        <Text
-                            fontSize={{ base: 'sm', md: 'md' }}
-                            as='p'
-                            fontWeight="normal"
-                            color='var(--clr-primary-1)'
-                        >
-                            <Text as="span" fontWeight="semibold">More details:</Text><br/>
-                           Entry per person comes with Entry and cocktail snacks, Table of 4 comes with Don Julio bottle + platter,
-                           and Table of 8 comes with Don Julio 1942 + platter
-                        </Text>
-
-                        <Text
-                            fontSize={{ base: 'sm', md: 'md' }}
-                            as='p'
-                            fontWeight="normal"
-                            color='var(--clr-primary-1)'
-                        >
-                            <Text as="span" fontWeight="semibold">Date:</Text> COMING SOON
-                        </Text>
-                    </Stack>
-
-                    <Box w={{ base: '100%', md: 'auto' }}>
-                        <Button
-                            as={Link}
-                            href="https://kayetickets.com/"
-                            target="_blank"
-                            size={{ base: 'md', md: 'lg' }}
-                            w={{ base: '100%', sm: 'auto' }}
-                            _hover={{
-                                                    textDecoration: "none",
-                                                    bg: "var(--clr-primary-1)",
-                                                    color: "black",
-                                                }}
-
-                                             referrerPolicy='no-referrer'
-                                                rounded="full"
-                                                variant="outline"
-
-                                                padding={2}
-                                                color="var(--clr-primary-1)"
-                        >
-                            GET TICKETS
-                            <FiChevronRight style={{ marginLeft: '8px' }} />
-                        </Button>
-                    </Box>
-                </VStack>
-            </Box>
-        </Stack>
-
-        {/* Ibiza Experience */}
-        <Stack
-            direction={{ base: 'column', lg: 'row' }}
-            spacing={{ base: '6', md: '8', lg: '20' }}
-            align={{ base: 'center', lg: 'stretch' }}
-            mt={{ base: '10', md: '14', lg: '16' }}
-        >
-            {/* Images Section */}
-            <Flex
-                flex="1"
-                overflow="hidden"
-                direction={{ base: 'column', sm: 'row' }}
-                gap={{ base: 4, sm: 0 }}
-                maxW={{ base: '100%', lg: 'none' }}
-                w="100%"
-            >
-                <Image
-                    src="/haul/ibiza.webp"
-                    alt="Ibiza Experience"
-                    fallback={<Skeleton />}
-                    rounded={{ base: '10', sm: '0' }}
-                    roundedLeft={{ sm: '10' }}
-                    h={{ base: '200px', sm: '300px', md: '350px', lg: '550px' }}
-                    w={{ base: '100%', sm: '50%' }}
-                    objectFit='cover'
-                    _hover={{ boxShadow: 'xl' }}
-                />
-                <Image
-                    display={{ base: 'none', sm: 'block' }}
-                    src="/haul/340805-Ibiza-Island.webp"
-                    alt="Ibiza Island"
-                    fallback={<Skeleton />}
-                    roundedRight="10"
-                    h={{ sm: '300px', md: '350px', lg: '550px' }}
-                    w={{ sm: '50%' }}
-                    objectFit='cover'
-                    _hover={{ boxShadow: 'xl' }}
                 />
             </Flex>
 
